d909cbebb0cb7141cbc2e8b56e9c5f943c4fe0ba
ahymery <ahymery22@gmail.com>
2023-12-09 21:54:27 +0100

    carrousel des livres (à relier a mysql)

---
+++ carousel.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<body>
+<div id="livres" class="carousel slide" data-ride="carousel">
+
+<!-- Indicators -->
+<ul class="carousel-indicators">
+  <li data-target="#livres" data-slide-to="0" class="active"></li>
+  <li data-target="#livres" data-slide-to="1"></li>
+  <li data-target="#livres" data-slide-to="2"></li>
+  <li data-target="#livres" data-slide-to="3"></li>
+</ul>
+
+<!-- contenu du carroussel -->
+<div class="carousel-inner">
+  <div class="carousel-item active">s
+    <img src="hp1.png" alt="Harry Potter">
+  </div>
+  <div class="carousel-item">
+    <img src="Le_Petit_Prince.png" alt="Le Petit Prince">
+  </div>
+  <div class="carousel-item">
+    <img src="neuromancien.png" alt="Neuromancien">
+  </div>
+  <div>
+    <img src="onepiece.png" alt="One Piece Tome 1">
+  </div>
+</div>
+
+<!-- Left and right controls -->
+<a class="carousel-control-prev" href="#livres" data-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</a>
+<a class="carousel-control-next" href="#livres" data-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</a>
+</div>
+</body>
+</html>
