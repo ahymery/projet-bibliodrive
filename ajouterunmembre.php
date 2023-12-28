307edebecbdb1d5f8512c9d7fbc29e768aaea56d
ahymery <148552312+ahymery@users.noreply.github.com>
2023-12-28 23:57:19 +0100

    Mise à jour 28/12

--- ajouterunmembre.php
+++ ajouterunmembre.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <title>BiblioDrive</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="images/favicon.png">
+  <link rel="icon" type="image/x-icon" href="favicon.png">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style2.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
@@ -34,43 +35,33 @@ img {
       border-radius: 15px;
       border-color: #046aaa;
   }
+  
 
-  h1, h6 {
-    text-align: center;
-  }
 </style>
 </head>
 <body>
-<div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-4 col-md-12">
-        <?php include("entete.html");?>
-      </div> 
-    </div>
-    <div class="row">
-      <div class="col-md-8">    
-          <form method="post">
-              <br><h1 style= "margin-left: 550px;"><strong>CRÉER UN MEMBRE</strong></h1><br>  
-              <label for="email" style= "margin-left: 750px;">Email :</label><br>
-              <input type="email" name="mel" placeholder="Mail" style= "margin-left: 750px;"><br>
-              <label for="password" style= "margin-left: 750px;">Mot de passe :</label><br>
-              <input type="password" name="motdepasse" placeholder="Mot de passe" style= "margin-left: 750px;"><br>
-              <label for="nom" style= "margin-left: 750px;">Nom :</label><br>
-              <input type="text" name="nom" placeholder="Nom" style= "margin-left: 750px;"><br>
-              <label for="prénom" style= "margin-left: 750px;">Prénom :</label><br>
-              <input type="text" name="prenom" placeholder="Prénom" style= "margin-left: 750px;"><br>
-              <label for="adresse" style= "margin-left: 750px;">Adresse :</label><br>
-              <input type="text" name="adresse" placeholder="Adresse postale" style= "margin-left: 750px;"><br>
-              <label for="ville" style= "margin-left: 750px;">Ville :</label><br>
-              <input type="text" name="ville" placeholder="Ville" style= "margin-left: 750px;"><br>
-              <label for="codepostal" style= "margin-left: 750px;">Code Postal :</label><br>
-              <input type="text" name="codepostal" placeholder="Code Postal" style= "margin-left: 750px;"><br>
-              <button type="submit" class="btn btn-outline-primary btn-sm" name="add" style= "margin-left: 750px;">
+    <?php include ('entete.html');?>
+<form method="post">
+    <br><h1 style= "margin-left: 550px;"><strong>AJOUTER UN MEMBRE</strong></h1><br>
+    <label for="mel" style="margin-left: 550px;">Mail :</label><br>
+    <input type="email" name="mel" style="margin-left: 550px;" placeholder="Mail"><br>
+    <label for="motdepasse" style= "margin-left: 550px;">Mot de passe :</label><br>
+    <input type="password" name="motdepasse" style= "margin-left: 550px;" placeholder="Mot de passe"><br>
+    <label for="nom" style= "margin-left: 550px;">Nom :</label><br>
+    <input type="text" name="nom" style= "margin-left: 550px;" placeholder="Nom"><br>
+    <label for="prenom" style= "margin-left: 550px;">Prénom :</label><br>
+    <input type="text" name="prenom" style= "margin-left: 550px;" placeholder="Prénom"><br>
+    <label for="adresse" style= "margin-left: 550px;">Adresse Postale :</label><br>
+    <input type="text" name="adresse" placeholder="Adresse" style= "margin-left: 550px;"></input><br>
+    <label for="ville" style= "margin-left: 550px;">Ville :</label><br>
+    <input type="text" name="ville"  style= "margin-left: 550px;" placeholder="Ville"><br>
+    <label for="codepostal" style= "margin-left: 550px;">Code postal :</label><br>
+    <input type="text" name="codepostal" placeholder="Code postal" style= "margin-left: 550px;">
+    <button type="submit" class="btn btn-outline-primary btn-sm" name="add">
                 <i class="fas fa-plus"></i> Ajouter un membre
               </button>
-          </form>
-      </div>   
-    </div>
+</form>
+</div>  
 
 <?php    
     if (isset($_REQUEST["nom"])) {
