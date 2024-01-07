05cf7a95427bc1435f2dcf5b1c25f68c04fcc35c
ahymery <148552312+ahymery@users.noreply.github.com>
2024-01-07 23:38:00 +0100

    MISE A JOUR 07/01/2024
    Ajout d'un carousel dynamique (connecté à la base sql)

--- accueil.php
+++ accueil.php
@@ -14,6 +14,12 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+   .carousel-inner .carousel-item img {
+      width: 250px;
+      margin-left: 370px;
+    }
+ </style>
 </head>
 <body>
   <?php
---
+++ carousel.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<body>
+<div id="livres" class="carousel slide" data-ride="carousel">
+
+
+<!-- contenu du carroussel -->
+<div class="carousel-inner">
+ <?php 
+
+// Connexion a la base de donnée
+  $servername = "localhost";  
+  $username = "root";
+  $password = "";
+  $dbname = "projet-bibliodrive";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verification de la connexion
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+    $sql = "SELECT nolivre, titre, photo FROM livre ORDER BY nolivre DESC LIMIT 5";
+    $result = $conn->query($sql);
+    $count = 0;
+    if ($result->num_rows > 0) {
+      
+      while($row = $result->fetch_assoc()) {
+        echo "<div class='carousel-item".($count == 0 ? " active" : "")."'><img src='covers/".$row["photo"]."' alt='".$row["titre"]."'></div>";
+        $count++;
+      }
+    } else {
+      echo "0 resultats";
+    }
+    $conn->close();
+ ?>
+</div>
+
+  <a class="carousel-control-prev" href="#livres" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#livres" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+</body>
+</html>
